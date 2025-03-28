9185808b595e7a2518d2ebf5aa89d893301b9dc1
Ahmed Alkaff <ahmedalkaff2006@yahoo.com>
2025-03-28 17:02:47 +0400

    Moved main menu into loop

--- src/thebill.c
+++ src/thebill.c
@@ -1,14 +1,66 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+#define TB_MM_NEW 1
+#define TB_MM_SHOWONE 2
+#define TB_MM_SHOWDATE 3
+#define TB_MM_SHOWDATERANGE 4
+#define TB_MM_EDIT 5
+#define TB_MM_DELETE 6
+#define TB_MM_EXIT 0
 
 int getProgramMode();
 void printMainMenu();
+bool runMainMenu();
 
 
 int main(void)
 {
-    int mode = getProgramMode();
+    bool running = true;
+
+    while (running)
+    {
+        running = runMainMenu();
+    }
 
     return 0;
+}
+
+bool runMainMenu()
+{
+    bool result = true;
+
+    int mode = getProgramMode();
+
+    switch (mode)
+    {
+        case TB_MM_NEW:
+            break;
+
+        case TB_MM_SHOWONE:
+            break;
+
+        case TB_MM_SHOWDATE:
+            break;
+
+        case TB_MM_SHOWDATERANGE:
+            break;
+
+        case TB_MM_EDIT:
+            break;
+
+        case TB_MM_DELETE:
+            break;
+
+        case TB_MM_EXIT:
+            result = false;
+            break;
+        
+        default:
+            break;
+    }
+
+    return result;
 }
 
 int getProgramMode()
